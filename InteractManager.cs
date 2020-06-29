518114f82fc337f17e4e631471f5475d7e91ae2f
jeed2424 <50756835+jeed2424@users.noreply.github.com>
2020-06-29 07:29:02 -0400

    Add files via upload

---
+++ InteractManager.cs
@@ -0,0 +1,113 @@
+/*
+InteractManager.cs - wirted by ThunderWire Games * Script for Interact Buttons, Items etc.
+*/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InteractManager : MonoBehaviour {
+	
+	public float PickupRange = 3; 
+	//public string UseButton = "Use";
+	
+	public GameObject playerCam;
+	private Ray playerAim;
+	
+	private bool isPressed;
+	private GameObject objectInteract;
+
+	public GameObject cHNormal;
+	public GameObject cHHighlight;
+
+
+	void Start () {
+		//playerCam = Camera.main.gameObject;
+		isPressed = false;
+		objectInteract = null;
+	}
+	
+	void Update () {
+		Ray playerAim = playerCam.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+		RaycastHit hit;
+
+		if (Physics.Raycast(playerAim, out hit, PickupRange))
+		{
+			
+			if (hit.collider.tag == "ElevatorUp")
+			{
+				objectInteract = hit.collider.gameObject;
+				CrosshairActive();
+
+				if (Input.GetKeyDown(KeyCode.E) && !isPressed)
+				{
+					InteractUse("ElevatorUp");
+					isPressed = true;
+				}
+				else if (Input.GetKeyDown(KeyCode.E) && isPressed)
+				{
+					isPressed = false;
+				}
+				
+			}
+			if (hit.collider.tag == "ElevatorDown")
+			{
+				objectInteract = hit.collider.gameObject;
+				CrosshairActive();
+				
+				if (Input.GetKeyDown(KeyCode.E) && !isPressed)
+				{
+					InteractUse("ElevatorDown");
+					isPressed = true;
+				}
+				else if (Input.GetKeyDown(KeyCode.E) && isPressed)
+				{
+					isPressed = false;
+				}
+			}
+		}
+		else
+		{
+			CrosshairNormal();
+			isPressed = false;
+		}
+	}
+	
+	//private void Interact(){
+		//Ray playerAim = playerCam.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+		//RaycastHit hit;
+		
+		//if (Physics.Raycast (playerAim, out hit, PickupRange)){
+			//objectInteract = hit.collider.gameObject;
+			//CrosshairActive();
+			//if(hit.collider.tag == "ElevatorUp"){
+			//	InteractUse("ElevatorUp");
+			//}
+			//if(hit.collider.tag == "ElevatorDown"){
+			//	InteractUse("ElevatorDown");
+			//}
+		//}
+        //else
+        //{
+			//CrosshairNormal();
+			//isPressed = false;
+        //}
+	//}
+	
+    private void InteractUse(string Call)
+    {
+		objectInteract.GetComponent<ElevatorButton>().SendCall(Call);
+    }
+
+	void CrosshairActive()
+	{
+		cHNormal.SetActive(false);
+		cHHighlight.SetActive(true);
+	}
+
+	void CrosshairNormal()
+	{
+		cHNormal.SetActive(true);
+		cHHighlight.SetActive(false);
+	}
+}
---
+++ RaycastManager_Lever.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastManager_Lever : MonoBehaviour
+{
+    private GameObject raycasted_obj;
+
+    private int rayLength = 5;
+    public LayerMask layerMaskInteract;
+
+    public GameObject cHNormal;
+    public GameObject cHHighlight;
+
+    private bool canPull = true;
+
+    
+    
+
+    
+
+    private LeverController leverController;
+
+    
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if(Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMaskInteract.value))
+        {
+            if (hit.collider.CompareTag("Lever"))
+            {
+                raycasted_obj = hit.collider.gameObject;
+                CrosshairActive();
+
+                if (Input.GetKeyDown("e") && canPull)
+                {
+                    raycasted_obj.GetComponentInChildren<Animator>().Play("HandlePull", -1, 0.0f);
+                    raycasted_obj.GetComponent<LeverScript>().LeverNumber();
+                    leverController = raycasted_obj.GetComponentInParent<LeverController>();
+                    StartCoroutine(Timer(1.0f));
+                  
+                }
+            } 
+
+            
+        }
+
+        else
+        {
+            CrosshairNormal();
+        }
+    }
+
+    void CrosshairActive()
+    {
+        cHNormal.SetActive(false);
+        cHHighlight.SetActive(true);
+    }
+
+    void CrosshairNormal()
+    {
+        cHNormal.SetActive(true);
+        cHHighlight.SetActive(false);
+    }
+
+    IEnumerator Timer(float waitTime)
+    {
+        canPull = false;
+        yield return new WaitForSeconds(waitTime);
+        canPull = true;
+    }
+
+    
+
+    
+}
